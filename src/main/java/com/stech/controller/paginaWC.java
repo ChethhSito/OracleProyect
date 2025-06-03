b81f91da9fdd2eaa000f3c38e6605630f7d9cfe5
Huayta <jdavidhuay@gmail.com>
2025-06-03 01:24:43 -0500

    listarTablas100%

--- src/main/java/com/stech/controller/paginaWC.java
+++ src/main/java/com/stech/controller/paginaWC.java
@@ -2,7 +2,6 @@ package com.stech.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -10,6 +9,10 @@ public class paginaWC {
     @GetMapping("/clientes")
     public String PageClients() {
         return "paginas/clientes";
+    }
+    @GetMapping("/direcciones")
+    public String PageAddresses() {
+        return "paginas/direcciones";
     }
     
 }
